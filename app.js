7f85ffdb5f341ae6f2b9cf84fef19fb6be7be927
Cory Chaplin <cory.o.chaplin@gmail.com>
2023-01-05 20:10:13 -0600

    finished dynamic routing with parameters

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank =require("./postBank");
+
 const app = express();
 
+app.use(express.static('public')) 
+app.use(morgan('dev'))
+
 app.get("/", (req, res) => 
-    res.send("Hello World!"
-));
+   {
+    const posts = postBank.list();
+
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${posts.map(post => `
+          <div class='news-item'>
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+              ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.date}
+            </small>
+          </div>`
+        ).join('')}
+      </div>
+    </body>
+  </html>`
+
+      res.send(html)
+   });
+
+
+
 
 const PORT = 1337;
 
