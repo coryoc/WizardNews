f94c527cddddef265bf96b94ac8b3091b12f3ccd
Cory Chaplin <cory.o.chaplin@gmail.com>
2023-01-07 11:39:10 -0600

    error handling investigated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) =>
           <div class='news-item'>
             <p>
               <span class="news-position">${post.id}. ▲</span>
-              ${post.title}
+              <a href="/posts/${post.id}">${post.title}</a>
               <small>(by ${post.name})</small>
             </p>
             <small class="news-info">
@@ -39,9 +39,50 @@ app.get("/", (req, res) =>
       res.send(html)
    });
 
+   app.get('/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const post = postBank.find(id);
+
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+          <div class='news-item'>
+            <p>
+              ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <p>
+            ${post.content}
+            </p>
+          </div>
+      </div>
+    </body>
+  </html>`
+
+
+    res.send(html);
+  });
 
 
 
+  app.get('/posts/:id', (req, res) => {
+    const id = req.params.id
+    const post = find(id)
+    if (!post.id) {
+      throw new Error('Not Found')
+    }
+  })
+  ;
+  // TODO - create error handler responding with 404 status code and Not Found after reading https://expressjs.com/en/guide/error-handling.html and associated resources
+
+
+ 
 const PORT = 1337;
 
 app.listen(PORT, () => {
